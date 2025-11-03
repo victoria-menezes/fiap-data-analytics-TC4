75021b8e809317d1efb48fbfce1937d9399a86a9
victoria-menezes <victoria.c.menezes@gmail.com>
2025-11-03 13:40:55 -0300

    Start pipeline conversion

---
+++ queries/create_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE obesity (
+    gender VARCHAR(25),
+    age INT,
+    height FLOAT,
+    weight FLOAT,
+    family_history BOOLEAN,
+    favc BOOLEAN,
+    fcvc INT,
+    ncp INT,
+    caec VARCHAR(255),
+    smoke BOOLEAN,
+    ch2o INT,
+    scc BOOLEAN,
+    faf INT,
+    tue INT,
+    calc VARCHAR(255),
+    mtrans VARCHAR(255)
+    obesity VARCHAR(255)
+)
